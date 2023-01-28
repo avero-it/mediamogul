1d7c9d690b2c5c32a612cd9818f491277a4c45f7
Simone Baglioni <github@simonebaglioni.com>
2023-01-28 15:35:33 +0100

    removed some stx leftovers

--- pkg/httpserver/routes.go
+++ pkg/httpserver/routes.go
@@ -4,5 +4,4 @@ import "net/http"
 
 func (srv server) Routes() {
 	srv.router.HandleFunc("/healthz", srv.healthz()).Methods(http.MethodGet)
-	srv.router.HandleFunc("/api/s3audiotonlp", srv.handleS3AudioToNLP()).Methods(http.MethodPost)
 }
