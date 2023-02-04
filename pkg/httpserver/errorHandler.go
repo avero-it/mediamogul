f9c37c48d6a3af427207d894bbbee3c2eab19862
Simone Baglioni <github@simonebaglioni.com>
2023-02-04 11:23:28 +0100

    rmvd pkg

--- pkg/httpserver/errorHandler.go
+++
@@ -1,7 +0,0 @@
-package httpserver
-
-import "net/http"
-
-func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
-	w.WriteHeader(status)
-}
--- pkg/httpserver/routes.go
+++
@@ -1,7 +0,0 @@
-package httpserver
-
-import "net/http"
-
-func (srv server) Routes() {
-	srv.router.HandleFunc("/healthz", srv.healthz()).Methods(http.MethodGet)
-}
